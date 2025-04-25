15d654c7cdd87b8f7617db99df77fb4fa4f9ed79
Mahendra931 <162870296+Mahendra931@users.noreply.github.com>
2025-04-25 09:36:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".text",{
+    strings:["DataScientist","Web Developer","Frontend Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
